5bc1241b13c833470b68872dd07277af8f6ea586
JB <108816562+GarethJB@users.noreply.github.com>
2023-03-31 18:13:43 +0900

    update

--- app/src/main/java/com/example/aop/part2/imagedisplay/models/PhotoResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.example.aop.part2.imagedisplay.models
-
-
-import com.google.gson.annotations.SerializedName
-
-class PhotoResponse : ArrayList<PhotoResponseItem>()
--- app/src/main/java/com/example/aop/part2/imagedisplay/models/Position.kt
+++
@@ -1,9 +0,0 @@
-package com.example.aop.part2.imagedisplay.models
-
-
-import com.google.gson.annotations.SerializedName
-
-data class Position(
-    val latitude: Double?,
-    val longitude: Double?
-)
